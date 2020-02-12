db5bf084afc3ab649fd0c8c423d0c290c5edb2f2
Lance Zotigh <lancezotighlz@gmail.com>
2020-02-12 12:47:45 -0700

    Add project files.

---
+++ VectorDemo/VectorDemo.cpp
@@ -0,0 +1,32 @@
+// VectorDemo.cpp 
+// LAnce Zotigh(lzotigh1@cnm.edu)
+// Date: 2/12/2020
+// Purpose: To demo vectors and random
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+int main()
+{
+    // Variables
+    vector<string> names;
+    string name;
+
+    // Start a do another loop
+    do {
+        //Get name from user
+        cout << "Please enter a name: ";
+        getline(cin, name);
+
+        // If user name is not "" add to vector
+        if (name != "") {
+            names.push_back(name);
+        }
+        // Loop back if input is not ""
+    } while (name != "");
+
+    //Loop through vector and display it
+    return 0;
+}
